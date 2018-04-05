15e2a32ffded62793dd77c83a97a8b56814b8486
Yael Goldin <ygoldin@uw.edu>
2018-04-05 13:12:45 -0700

    can now check if any pile is empty and move cards between the stock/waste

--- src/gameplay/SolitaireModel.java
+++ src/gameplay/SolitaireModel.java
@@ -12,7 +12,7 @@ public class SolitaireModel {
 	private TableauPile[] tableau;
 	private Foundation[] foundations;
 	
-	private static final int TABLEAU_SIZE = 7;
+	public static final int TABLEAU_SIZE = 7;
 	
 	/**
 	 * initializes the solitaire model
@@ -34,6 +34,82 @@ public class SolitaireModel {
 			foundations[i] = new Foundation(suits[i]);
 		}
 	}
+	
+	/**
+	 * moves all of the cards from the waste pile into the stock pile
+	 * 
+	 * @throws IllegalStateException if the stock isn't empty or the waste is empty
+	 */
+	public void moveWasteToStock() {
+		if(!stockIsEmpty()) {
+			throw new IllegalStateException("stock is not empty");
+		} else if(wasteIsEmpty()) {
+			throw new IllegalStateException("waste is empty");
+		}
+		stock.moveCardsFromWaste();
+	}
+	
+	/**
+	 * moves the top card from the stock pile onto the waste pile
+	 * 
+	 * @throws IllegalStateException if the stock is empty
+	 */
+	public void moveTopStockCardToWaste() {
+		if(stockIsEmpty()) {
+			throw new IllegalStateException("stock is empty");
+		}
+		stock.removeTopCard();
+	}
+	
+	/* ************************check if any pile is empty*************************************** */
+	
+	/**
+	 * checks if the stock is empty
+	 * 
+	 * @return true if it is, false otherwise
+	 */
+	public boolean stockIsEmpty() {
+		return stock.isEmpty();
+	}
+	
+	/**
+	 * checks if the waste pile is empty
+	 * 
+	 * @return true if it is, false otherwise
+	 */
+	public boolean wasteIsEmpty() {
+		return waste.isEmpty();
+	}
+	
+	/**
+	 * checks if the given tableau pile is empty
+	 * 
+	 * @param index The index of which tableau pile to look at
+	 * @return true if the pile is empty, false otherwise
+	 * @throws IllegalArgumentException if index is outside of the range [0, TABLEAU_SIZE] inclusive
+	 */
+	public boolean tableauPileIsEmpty(int index) {
+		if(index < 0 || index > tableau.length) {
+			throw new IllegalArgumentException("invalid tableau pile index");
+		}
+		return tableau[index].isEmpty();
+	}
+	
+	/**
+	 * checks if the given foundation is empty
+	 * 
+	 * @param index The index of which foundation to look at
+	 * @return true if the foundation is empty, false otherwise
+	 * @throws IllegalArgumentException if index is outside of the range [0, 3] inclusive
+	 */
+	public boolean foundationIsEmpty(int index) {
+		if(index < 0 || index > foundations.length) {
+			throw new IllegalArgumentException("invalid foundation index");
+		}
+		return foundations[index].isEmpty();
+	}
+	
+	/* ***************************************************************************************** */
 	
 	/**
 	 * checks if the game is over
